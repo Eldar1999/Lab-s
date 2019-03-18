125846ed1108ea2378bf019d4abfbcfbdef80b98
Eldar1999 <abibulaev.eldar@gmail.com>
2019-03-19 01:18:06 +0300

    update

--- solution.c
+++ solution.c
@@ -8,54 +8,53 @@ typedef struct dirent* dirent;
 
 void print(char* path)
 {
-    FILE* sol = fopen("result.txt","a");
-    fprintf(sol,"%s",path);
+	FILE* sol = fopen("result.txt","a");
+	fprintf(sol,"%s",path);
 }
 
 void search(char* path,char* filename)
 {
 	DIR* base = opendir(path);
-
 	FILE* file;
-        if((file = fopen(filename,"r")))
+	if((file = fopen(filename,"r")))
+	{
+		char* s = malloc(sizeof(char)*80);
+		do
 		{
-			char* files;
-			char* s = malloc(sizeof(char)*80);
-			do
+			fgets(s,80,file);
+			s = strtok(s," ");
+			s = strtok(s," ");
+			if(!strtok(s,"Deadlock"))
 			{
-				fgets(s,80,file);
-				s = strtok(s," ");
-				s = strtok(s," ");
-				if(!strtok(s,"Deadlock"))
-				{
-					print(strcat(strcat(path,"/"),filename));
-					return;
-				}
-				else if (!strtok(s,"Minotaur"))
-				{
-					print(strcat(strcat(path,"/"),filename));
-					exit(0);
-				}
-				else
-				{
-					search(path,s);
-				}
-			}while(feof(file));
-		}
+				print(strcat(strcat(path,"/"),filename));
+				return;
+			}
+			else if (!strtok(s,"Minotaur"))
+			{
+				print(strcat(strcat(path,"/"),filename));
+				exit(0);
+			}
+			else
+			{
+				search(path,s);
+			}
+		}while(feof(file));
+	}
 	else
 	{
 		dirent direct;
 		while((direct = readdir(base)))
 		{
-			if(direct.d_type == 4)
+			if(direct->d_type == 4 && !strcmp(direct->d_name,".") && !strcmp(direct->d_name,".."))
 			{
-				search(strcat(strcat(path,"/"),direct->d_name),filename);
+				path[strlen(path)+1]='\0';
+				path[strlen(path)]='/';
+				search(strcat(path,direct->d_name),filename);
+				path[strlen(path)-1] = '\0';
 			}
 		}
 	}
 }
-
-
 
 int main()
 {
