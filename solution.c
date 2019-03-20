bed8703e677514e6998504377c7738c455d20cf7
Eldar <abibulaev.eldar@gmail.com>
2019-03-21 01:50:53 +0300

    UPDATE

--- solution.c
+++ solution.c
@@ -4,59 +4,72 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-typedef struct dirent* dirent;
+#define HOMEDIR '.'
 
-void print(char* path)
-{
-	FILE* sol = fopen("result.txt","a");
-	fprintf(sol,"%s",path);
+char path[3000] = ".", filename[30] = "file.txt", ex1[2] = ".\0", ex2[3] = "..\0", min[] = "Minotaur", bl[] = "Deadlock";
+
+typedef struct dirent *direct;
+
+void print(char *path) {
+    FILE *sol = fopen("result.txt", "a");
+    fprintf(sol, "%s\n", path);
+    fclose(sol);
 }
 
-void search(char* path,char* filename)
-{
-	DIR* base = opendir(path);
-	FILE* file;
-	if((file = fopen(filename,"r")))
-	{
-		char* s = malloc(sizeof(char)*80);
-		do
-		{
-			fgets(s,80,file);
-			s = strtok(s," ");
-			s = strtok(s," ");
-			if(!strtok(s,"Deadlock"))
-			{
-				print(strcat(strcat(path,"/"),filename));
-				return;
-			}
-			else if (!strtok(s,"Minotaur"))
-			{
-				print(strcat(strcat(path,"/"),filename));
-				exit(0);
-			}
-			else
-			{
-				search(path,s);
-			}
-		}while(feof(file));
-	}
-	else
-	{
-		dirent direct;
-		while((direct = readdir(base)))
-		{
-			if(direct->d_type == 4 && !strcmp(direct->d_name,".") && !strcmp(direct->d_name,".."))
-			{
-				path[strlen(path)+1]='\0';
-				path[strlen(path)]='/';
-				search(strcat(path,direct->d_name),filename);
-				path[strlen(path)-1] = '\0';
-			}
-		}
-	}
+char *createname() {
+    char *a = calloc(strlen(path) + strlen(filename) + 1, sizeof(char));
+    char d[2] = "/\0";
+    strcat(a, path);
+    strcat(a, d);
+    strcat(a, filename);
+    return a;
 }
 
-int main()
-{
-	search(".","file.txt");
+void search() {
+    DIR *dir = opendir(path);
+    char *tmp;
+    int a;
+
+    tmp = createname(path, filename);
+    FILE *doc;
+    if ((doc = fopen(tmp, "r"))) {
+        print(tmp);
+        free(tmp);
+        char buf[80];
+        while (!feof(doc)) {
+            fgets(buf, 80, doc);
+            strcpy(filename,strtok(buf, " \n\0"));
+            if (!strcmp(filename, min)) {
+                exit(0);
+            } else if (!strcmp(filename, bl)) {
+                free(tmp);
+                path[0] = HOMEDIR;
+                path[1] = 0;
+                return;
+            }
+            strcpy(filename,strtok(NULL, " \n\0"));
+            path[0] = HOMEDIR;
+            path[1] = 0;
+            search();
+        }
+    } else {
+        direct files;
+        while ((files = readdir(dir))) {
+            if (files->d_type == DT_DIR && strcmp(files->d_name, ex1) && strcmp(files->d_name, ex2)) {
+                a = strlen(path);
+                path[strlen(path) + 1] = '\0';
+                path[strlen(path)] = '/';
+                strcat(path, files->d_name);
+                search();
+                path[a] = '\0';
+            }
+        }
+    }
+    free(tmp);
+}
+
+int main() {
+    FILE *a = fopen("result.txt", "w");
+    fclose(a);
+    search(path, filename);
 }
